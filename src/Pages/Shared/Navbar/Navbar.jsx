94708c080f5f239dd91e484558176a08294fc50c
dronatoo <mrdronatoo@gmail.com>
2023-11-27 14:21:40 -0800

    added navbar

---
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    const links = <>
+        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/">Home</NavLink></li>
+        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/donationRequest">Donation Request</NavLink></li>
+        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/blog">Blog</NavLink></li>
+        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/registration">Registration</NavLink></li>
+        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/dashboard">Registration</NavLink></li>
+        <li><NavLink className="rounded-none" style={({ isActive })=> ({borderBottom: isActive ? "2px solid white" : " ", background: "transparent",})} to="/fundings">Fundings</NavLink></li>
+    </>
+    return (
+        <div>
+            <div className="navbar bg-gradient-to-r from-[#0a3d62] to bg-[#b33939] text-white py-5 px-10">
+              <div className="navbar-start">
+                <div className="dropdown">
+                  <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                  </div>
+                  <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 text-black shadow bg-base-100 rounded-box w-52">
+                    {links}
+                  </ul>
+                </div>
+                <div className="flex items-center">
+                    <img className="w-14" src="https://i.ibb.co/SR7G805/bos.png" alt="logo"/>
+                    <h1 className="text-2xl md:text-3xl font-bold ml-2">Blood Bucket</h1>
+                </div>
+              </div>
+              <div className="navbar-end">
+              <div className="hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                  {links}
+                </ul>
+              </div>
+                <button className="btn btn-outline border-white text-white">Login</button>
+              </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import {
   } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home/Home";
+import Footer from "../Pages/Shared/Footer/Footer";
   
 export const router = createBrowserRouter([
     {
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
         {
             path: '/',
             element: <Home/>
+        },
+        {
+          path: '/blog',
+          element: <Footer/>  
         }
       ]
     },
