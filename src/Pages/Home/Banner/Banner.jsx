f46e7cf4763ef376f2186e19ed81a3879b79fc66
dronatoo <mrdronatoo@gmail.com>
2023-11-27 15:25:01 -0800

    added banner to home page

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,12 +1,12 @@
 const Banner = () => {
     return (
         <div>
-            <div className="hero h-[250px] md:h-[400px] lg:h-[600px]" style={{backgroundImage: 'url(https://i.ibb.co/0ZxSTfq/12.jpg)'}}>
-                <div className="hero-overlay bg-opacity-10"></div>
-                <div className="w-full px-10">
-                  <h1 className="mb-5 text-xl md:text-2xl lg:text-5xl font-bold text-[#b33939]"><i>A blood bag in time<span className="text-[#0a3d62]"> saves a life.</span></i></h1>
-                  <button className="btn bg-[#0a3d62] text-white">Join as a donor</button>
-                  <button className="btn bg-[#b33939] text-white ml-5">Search Donors</button>
+            <div className="hero h-[200px] md:h-[400px] lg:h-[600px]" style={{backgroundImage: 'url(https://i.ibb.co/0ZxSTfq/12.jpg)'}}>
+                <div className="hero-overlay bg-opacity-5"></div>
+                <div className="w-full px-4 md:px-10">
+                  <h1 className="mb-5 text-lg md:text-3xl lg:text-5xl font-bold text-[#b33939]"><i>A blood bag in time<span className="text-[#0a3d62]"> saves a life.</span></i></h1>
+                  <button className="btn w-24 md:w-auto bg-[#0a3d62] text-white">Join as a donor</button>
+                  <button className="btn w-24 md:w-auto bg-[#b33939] text-white ml-5">Search Donors</button>
                 </div>
             </div>
         </div>
