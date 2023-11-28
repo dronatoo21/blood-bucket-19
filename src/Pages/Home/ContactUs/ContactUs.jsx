c9b6553ff1117eef216ccff6306a2b788069f7e9
dronatoo <mrdronatoo@gmail.com>
2023-11-27 16:03:29 -0800

    added contact us section in home page

---
+++ src/Pages/Home/ContactUs/ContactUs.jsx
@@ -0,0 +1,40 @@
+import Lottie from "lottie-react";
+import paperPlane from "../../../assets/paperPlane.json"
+import { FaPhone } from "react-icons/fa";
+
+const ContactUs = () => {
+    return (
+        <div className="lg:p-0 md:p-0 p-5">
+            <div className="hero lg:h-[650px] bg-gradient-to-r from-[#b33939] to bg-[#0a3d62] rounded-lg md:my-10">
+              <div className="hero-content flex-col lg:flex-row">
+                <div className="text-center lg:text-left ">
+                  <Lottie className="md:h-[400px] lg:h-[600px]" animationData={paperPlane}/>
+                </div>               
+                <div className="flex-shrink-0 w-full rounded-tl-[50px] rounded-br-[50px] max-w-sm  lg:m-14">
+                  <form className="card-body">
+                    <h1 className="font-semibold text-white mb-3">Contact US</h1>
+                    <div className="form-control">
+                      <input type="text" placeholder="Your Name" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                      <input type="email" placeholder="Your E-mail" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                    <textarea type="text" placeholder="Your Message" name="description" className="input input-bordered pt-3 h-20" required />
+                    </div>
+                    <div className="form-control mt-6">
+                      <button className="btn bg-[#b33939] text-white border-none">Send Message</button>
+                    </div>
+                    <div className="flex items-center gap-3 text-white mt-5">
+                        <FaPhone/>
+                        <p className="text-white">Call us on : <span>88242###</span></p>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+        </div>
+    );
+};
+
+export default ContactUs;
--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,12 @@
 import Banner from "../Banner/Banner";
+import ContactUs from "../ContactUs/ContactUs";
 
 const Home = () => {
     return (
         <div>
             <Banner/>
+            <h1 className="font-bold text-3xl text-center mt-14 mb-2">Contact Us!</h1>
+            <ContactUs/>
         </div>
     );
 };
