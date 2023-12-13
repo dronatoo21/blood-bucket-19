607ad9bbfd2510fdd5e7cea95246c544d81cb782
dronatoo <mrdronatoo@gmail.com>
2023-12-13 06:43:13 -0800

    added messenger chatbot

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "../Banner/Banner";
 import ContactUs from "../ContactUs/ContactUs";
 import TopDonors from "../TopDonors/TopDonors";
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 const Home = () => {
     return (
@@ -10,6 +11,10 @@ const Home = () => {
             <TopDonors/>
             <h1 className="font-bold text-3xl text-center mt-14 mb-2">Contact Us!</h1>
             <ContactUs/>
+            <MessengerCustomerChat
+               pageId="180357251829446"
+               appId="1003824134021262"
+             />,
         </div>
     );
 };
