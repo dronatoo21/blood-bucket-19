3c3456c899f6ec9cf9d78efcb16045eecb561906
dronatoo <mrdronatoo@gmail.com>
2024-06-02 15:05:36 -0700

    changed home page titles

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -7,9 +7,9 @@ const Home = () => {
     return (
         <div>
             <Banner/>
-            <h1 className="font-bold text-3xl text-center mt-14 mb-2">Contact Us!</h1>
+            <h1 className="font-bold text-xl mt-7 mb-5 md:text-2xl lg:text-3xl text-center">Contact Us!</h1>
             <ContactUs/>
-            <h1 className="font-bold text-3xl text-center mt-14 mb-2">Top Donors!</h1>
+            <h1 className="font-bold text-xl mt-7 mb-5 md:text-2xl lg:text-3xl text-center">Top Donors!</h1>
             <TopDonors/>
             <MessengerCustomerChat
                pageId="206269595893067"
